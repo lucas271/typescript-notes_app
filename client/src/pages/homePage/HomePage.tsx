2d5f83a66f148c20ea21d96fc2eb150566a03670
Lucas <lapmrj@gmail.com>
2022-09-11 19:30:38 -0300

    notes_redux_implementation

--- client/src/pages/homePage/HomePage.tsx
+++ client/src/pages/homePage/HomePage.tsx
@@ -1,89 +1,102 @@
-import {AiOutlineSearch} from 'react-icons/ai'
+import { AiOutlineSearch } from 'react-icons/ai'
 
 import './styles/style.scss'
-import {Note} from '../../models/note'
+import React, { FormEvent, useEffect, useState } from 'react'
+import { Note } from '../../models/note'
 import SingleNote from '../../components/singleNote/SingleNote'
-import { useState } from 'react'
 
-const Notes: Note[]= [
-    {
-        title: 'yatar aviões ',
-        text: 'matar aviões as 2 da manha com uma faca de caça',
-        date: new Date(),
-        done: true,
-        id: 'odkoaskdbzsoa'
-    },
-    {
-        title: 'matar aviões ',
-        text: 'matar aviões as 2 da manha com uma faca de caça',
-        date: new Date(),
-        done: false,
-        id: 'odsadkoakghcvbskdoa'
+import { useDispatch, useSelector } from 'react-redux'
+import { AppThunkDispatch, RootState } from '../../services/store'
+import { createNoteThunk, NotesType } from '../../services/features/notes/notesSlice'
+import NoteForm from '../../components/noteForm/NoteForm'
 
-    },
-    {
-        title: 'matar aviões ',
-        text: 'matar aviões as 2 da manha com uma faca de caça',
-        date: new Date(),
-        done: false,
-        id: 'odkoasdac321zskdoa'
-    },
-    {
-        title: 'matar aviões ',
-        text: 'matar aviões as 2 da manha com uma faca de caça',
-        date: new Date(),
-        done: true,
-        id: 'odkosda21asfakdoa'
-    },
-    {
-        title: 'matar aviões ',
-        text: 'matar aviões as 2 da manha com uma faca de caça',
-        date: new Date(),
-        done: false,
-        id: 'odkoasdaczsdaskdoa'
-    },
-    {
-        title: 'matar aviões ',
-        text: 'matar aviões as 2 da manha com uma faca de caça',
-        date: new Date(),
-        done: true,
-        id: 'odzxckosda21askdoa'
-    },
-    {
-        title: 'matar aviões ',
-        text: 'matar aviões as 2 da manha com uma faca de caça',
-        date: new Date(),
-        done: false,
-        id: 'odko654asdaczskdoa'
-    },
-]
+const HomePage = () => {
+  const [searchNote, setSearchNote] = useState<string>('')
+  const [done, setDone] = useState<boolean>(false)
+  const [isForm, setIsForm] = useState<boolean>(false)
+  const [showError, setShowError] = useState<boolean>(false)
 
-const HomePage: React.FC = () => {
-    const [searchNote, setSearchNote] = useState<string>('')
-    const [done, setDone] = useState<boolean>(false)
+  useEffect(() => {
+    console.log('a')
+    setTimeout(() => {
+      setShowError(false)
+    }, 10000)
+  }, [showError])
 
-    const notesFilter = (notes: Note[]): Note[] => {
-        return notes.filter(note => note.done === done)
-        .filter(note => note.title.includes(searchNote) || note.text.includes(searchNote))
-    }
+  const dispatch = useDispatch<AppThunkDispatch>()
 
-    return <>
-        <div className='home_container'>
-            <div className='search_bar'>
-                <input type="text" placeholder='Search Note' onChange={(e) => setSearchNote(e.target.value)} value={searchNote}/>
-                <AiOutlineSearch className='search_icon'/>
-            </div>
-            <div className='filter_by_type'>
-                <label htmlFor="done">Done</label>
-                <input type="checkbox" id='done' checked={done} onChange={(() => setDone(!done))}/>
-            </div>
-            <main className='notes_container'>
-                {notesFilter(Notes).map(note => {
-                    return <SingleNote notes={Notes} note={note} key={note.id}/>
-                })}
-            </main>    
-        </div>    
+  // eslint-disable-next-line no-unused-vars
+  const noteSelector = useSelector<RootState, NotesType>(state => state.notesSlice)
+
+  const user = JSON.parse(String(localStorage.getItem('user')))
+  const Notes = user?.notes
+
+  if (!user) location.replace('/userAuth')
+
+  const handleForm = (e: FormEvent<HTMLFormElement>, title: string, text: string, setTitle: React.Dispatch<React.SetStateAction<string>>, setText:React.Dispatch<React.SetStateAction<string>>): void => {
+    e.preventDefault()
+
+    if (!title) return alert('field TITLE is EMPTY')
+    if (!text) return alert('field TEXT is EMPTY')
+
+    dispatch(createNoteThunk({ title, text }))
+    setTitle('')
+    setText('')
+    setIsForm(!isForm)
+    setShowError(true)
+  }
+
+  const notesFilter = (notes: Note[]): Note[] => {
+    return notes
+      .filter((note) => note.done === done)
+      .filter(
+        (note) =>
+          note.title.includes(searchNote) || note.text.includes(searchNote)
+      )
+  }
+
+  return (
+    <>
+      {noteSelector?.errors.length > 0 && showError
+        ? <div className='error_container'><span className='error'>{noteSelector.errors.map((error) => <p>{error}</p>)}</span></div>
+        : ''}
+      <div className="home_container">
+        <div className="search_bar">
+          <input
+            type="text"
+            placeholder="Search Note"
+            onChange={(e) => setSearchNote(e.target.value)}
+            value={searchNote}
+          />
+          <AiOutlineSearch className="search_icon" />
+        </div>
+        <div className="filter_by_type">
+          <label htmlFor="done">Done</label>
+          <input
+            type="checkbox"
+            id="done"
+            checked={done}
+            onChange={() => setDone(!done)}
+          />
+        </div>
+        <main className="notes_container">
+          <div className='note new_note'>
+            {!isForm
+              ? <>
+              <button onClick={() => setIsForm(!isForm)}>
+                +
+              </button>
+            </>
+              : <NoteForm handleForm={handleForm} isForm={isForm} setIsForm={setIsForm}/>
+            }
+          </div>
+          {notesFilter(Notes).map((note, index) => {
+            return <SingleNote note={note} key={note._id || index} setShowError={setShowError}/>
+          })}
+        </main>
+      </div>
     </>
+  )
 }
 
 export default HomePage
