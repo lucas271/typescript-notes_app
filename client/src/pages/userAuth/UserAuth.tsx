12d047dba65835ecabb5139bdd564628829faa72
Lucas <lapmrj@gmail.com>
2022-09-11 19:18:40 -0300

    add redux logic to userAuth

--- client/src/pages/userAuth/UserAuth.tsx
+++ client/src/pages/userAuth/UserAuth.tsx
@@ -1,78 +1,49 @@
-import { motion } from "framer-motion"
-import { useState } from "react"
-import { Navigate } from "react-router-dom"
+import { motion } from 'framer-motion'
+import { useState } from 'react'
+import { useSelector } from 'react-redux'
+import LoginComponent from '../../components/LoginComponent'
+import RegisterComponent from '../../components/RegisterComponent'
+import { UserType } from '../../services/features/user/userSlicer'
+import { RootState } from '../../services/store'
 import './styles/style.scss'
 
-const UserAuth = ({user}: {user: boolean}) => {
-    const [password, setPassword] = useState<string>('')
-    const [repeatPassword, setRepeatPassword] = useState<string>('')
-    const [username, setUsername] = useState<string>('')
+const UserAuth = () => {
+  const user = useSelector<RootState, UserType>(state => state.userSlice)
+  const [isLogin, setIsLogin] = useState<boolean>(true)
+  const isUser = localStorage.getItem('user')
 
-    const [isLogin, setIsLogin] = useState<boolean>(true)
+  const handleIsLoginChange = (): void => {
+    setIsLogin(!isLogin)
+  }
 
+  const validation = (username:string, password:string, repeatPassword?:string): string[] => {
+    if (!username || !password) return ['Empty fields']
 
-    const handleIsLoginChange = ():void => {
-        setPassword('')
-        setRepeatPassword('')
-        setUsername('')
-        setIsLogin(!isLogin)
-    }
+    if (password.length > 20) return ['password cannot be longer than 20 chars']
+    if (password.length < 6) return ['password cannot be smaller than 20 chars']
 
-    //handle logged user
-    if(!user) <Navigate to='/'/>
+    // validation for register
+    if (typeof repeatPassword !== 'string') return []
+    if (repeatPassword !== password) return ['passwords must match']
+    return []
+  }
 
-    return <motion.div 
-    initial={{opacity: 0}} 
-    animate={{opacity: 1, transition: {duration: 1}}} 
-    exit={{opacity: 0}}
-    className='auth_container'
+  if (isUser) location.replace('/')
+
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, transition: { duration: 1 } }}
+      exit={{ opacity: 0 }}
+      className="auth_container"
     >
-        {isLogin ? 
-        
-        //sign in
-        <div className='auth_form_container auth_form_container_left'>
-            <h2>Logar</h2>
-            <form action="" className="auth_form">
-                <div>
-                    <label htmlFor="">Usuário:</label>
-                    <input className="input_field" type="text" onChange={(e) => setUsername(e.target.value)} value={username}/>
-                </div>
 
-                <div>
-                    <label htmlFor="">Senha:</label>
-                    <input className="input_field" type="text" onChange={(e) => setPassword(e.target.value)} value={password}/>
-                </div>
-                <button className="form_btn">Entrar</button>
-            </form>
-            <p>Não tem uma conta? <span onClick={handleIsLoginChange}>Cadastre-se</span></p>
-        </div>
-        : 
-
-        //sign up
-        <div className="auth_form_container auth_form_container_right">
-            <h2>Registrar</h2>
-            <form action="" className="auth_form">
-                <div>
-                    <label htmlFor="">Usuário:</label>
-                    <input className="input_field" type="text" onChange={(e) => setUsername(e.target.value)} value={username}/>
-                </div>
-                <div>
-                    <label htmlFor="">Senha:</label>
-                    <input className="input_field" type="text" onChange={(e) => setPassword(e.target.value)} value={password}/>
-                </div>
-                <div>
-                    <label htmlFor="">Repetir senha: </label>
-                    <input className="input_field" type="text" onChange={(e) => setRepeatPassword(e.target.value)} value={repeatPassword}/>
-                </div>
-                <button className="form_btn">Cadastrar</button>
-            </form>
-            <p className="form_change">
-                Tem uma conta? <span onClick={handleIsLoginChange}>Logue!</span>
-            </p>
-
-        </div>
-        }
+      {isLogin
+        ? <LoginComponent user={user} validation={validation} handleIsLoginChange={handleIsLoginChange}/>
+        : <RegisterComponent user={user} validation={validation} handleIsLoginChange={handleIsLoginChange}/>
+      }
     </motion.div>
+  )
 }
 
 export default UserAuth
