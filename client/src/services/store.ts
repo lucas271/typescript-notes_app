8c90ff91eb2b8c95706aac5c2d47d57b438c8fa1
Lucas <lapmrj@gmail.com>
2022-09-11 19:17:38 -0300

    new reducers to store

--- client/src/services/store.ts
+++ client/src/services/store.ts
@@ -1,13 +1,16 @@
-import {configureStore} from '@reduxjs/toolkit'
-import thunk from 'redux-thunk'
+import { AnyAction, configureStore, Store, ThunkDispatch } from '@reduxjs/toolkit'
+import userSlice from './features/user/userSlicer'
+import notesSlice from './features/notes/notesSlice'
 
-const rootReducer = {
+const rootReducer = { userSlice, notesSlice }
 
-}
-
-const store = configureStore<any>({
-    reducer: rootReducer,
-
+const store = configureStore({
+  reducer: rootReducer
 })
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppThunkDispatch = ThunkDispatch<RootState, unknown, AnyAction>
+export type AppStore = Omit<Store<RootState, AnyAction>, 'dispatch'> & {dispatch: AppThunkDispatch}
 
 export default store
