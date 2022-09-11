1497e732871101de4f57c18a92c28f3c5edc3d04
Lucas <lapmrj@gmail.com>
2022-09-11 19:24:32 -0300

    redux implementation && styles change

--- client/src/pages/page404/page404.tsx
+++ client/src/pages/page404/page404.tsx
@@ -1,19 +1,21 @@
-import { Link, useLocation } from "react-router-dom"
-import {CgSmileSad} from 'react-icons/cg'
+import { Link, useLocation } from 'react-router-dom'
+import { CgSmileSad } from 'react-icons/cg'
 import './styles/style.scss'
 
-const Page404: React.FC = () => {
-
-    const location = useLocation()
-    console.log(location)
-    return <>
-        <article className="page404_container">
-            <CgSmileSad className="icon"/>
-            <h2>404 error</h2>
-            <p>It was not possible to find the path: {location.pathname}</p>
-            <Link to='/' className="go_home">Go back to home</Link>
-        </article>
+const Page404 = () => {
+  const location = useLocation()
+  return (
+    <>
+      <article className="page404_container">
+        <CgSmileSad className="icon" />
+        <h2>404 error</h2>
+        <p>It was not possible to find the path: {location.pathname}</p>
+        <Link to="/" className="go_home">
+          Go back to home
+        </Link>
+      </article>
     </>
+  )
 }
 
 export default Page404
